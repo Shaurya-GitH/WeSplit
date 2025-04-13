1c029767359961195ea5acc7f6d16e52ede36d3d
Shaurya-GitH <mehta.shaurya123@gmail.com>
2025-04-13 18:39:46 +0530

    used environment variable for key

--- src/main/java/com/wesplit/main/utils/JwtUtils.java
+++ src/main/java/com/wesplit/main/utils/JwtUtils.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -14,7 +15,8 @@ import java.util.Map;
 @Component
 public class JwtUtils {
 
-    private String SECRET_KEY = "TaK+HaV^uvCHEFsEVfypW#7g9^k*Z8$V";
+    @Value("${myapp.secret}")
+    private String SECRET_KEY;
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
