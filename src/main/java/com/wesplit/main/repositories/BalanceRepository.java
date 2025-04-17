618a13221bf8a70f5c68f607d5c63cd32df9e4d1
Shaurya-GitH <mehta.shaurya123@gmail.com>
2025-04-17 21:19:53 +0530

    added pessimistic locking for balance modification

--- src/main/java/com/wesplit/main/repositories/BalanceRepository.java
+++ src/main/java/com/wesplit/main/repositories/BalanceRepository.java
@@ -2,12 +2,16 @@ package com.wesplit.main.repositories;
 
 import com.wesplit.main.entities.Balance;
 import com.wesplit.main.entities.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface BalanceRepository extends JpaRepository<Balance,Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Balance> findByUser1AndUser2(User user1, User user2);
 }
