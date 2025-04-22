db9004547e1aeb419056385b62a164753187d6af
Shaurya-GitH <mehta.shaurya123@gmail.com>
2025-04-22 13:51:17 +0000

    added health-check API

--- src/main/java/com/wesplit/main/controllers/PublicController.java
+++ src/main/java/com/wesplit/main/controllers/PublicController.java
@@ -12,10 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/public")
@@ -47,5 +44,11 @@ public class    PublicController {
         UserDetails userDetails=userDetailsService.loadUserByUsername(loginUser.getEmail());
         String token= jwtUtil.generateToken(userDetails.getUsername());
         return ResponseEntity.ok().body(token);
+    }
+
+    //API healthCheck
+    @GetMapping("/health-check")
+    ResponseEntity<?> healthCheck(){
+        return ResponseEntity.ok().build();
     }
 }
